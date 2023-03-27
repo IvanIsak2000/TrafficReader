e9e96f6c3c451d90b423dfd872d572c34bad7b65
IvanIsak2000 <vingving648@gmail.com>
2023-03-27 16:30:30 +0300

    add database for save data

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-import math
+#!/usr/bin/env python3
 import time
 import collections
 import threading
 import pdb
+import sqlite3
+from datetime import datetime
+import pytz
 
 import dearpygui.dearpygui as dpg
 import psutil
 
+
+with sqlite3.connect("network_history.db") as db:  # create db for program
+
+    cursor = db.cursor()
+    query_start = """ CREATE TABLE IF NOT EXISTS network(
+        KB_per_second TEXT,
+        recording_time TEXT
+        ) """
+    cursor.execute(query_start)
 
 nsamples = 100
 
@@ -16,7 +28,7 @@ data_y = [0.0] * nsamples
 data_x = [0.0] * nsamples
 
 
-UPDATE_DELAY = 1
+UPDATE_DELAY = 0.1
 
 
 def get_net_speed():
@@ -55,10 +67,19 @@ def update_data():
         time.sleep(0.01)
         sample = sample + 1
 
+        recording_time = datetime.now(pytz.timezone(
+            'Europe/Moscow')).strftime("%H:%M:%S %Y-%m-%d")  # UTC+3
+
+        with sqlite3.connect("network_history.db") as db:
+            cursor = db.cursor()
+            query = """INSERT INTO network(KB_per_second,recording_time) VALUES(?,?) """
+            data = [(str(get_net_speed()[1]), str(recording_time))]
+            cursor.executemany(query, data)
+
 
 dpg.create_context()
 dpg.create_viewport(title='network-speed', width=850, height=640)
-with dpg.window(label='', width=800, height=600, tag = 'Primary Window'):
+with dpg.window(label='', width=800, height=600, tag='Primary Window'):
 
     with dpg.plot(label='Speed', height=-1, width=-1):
 
@@ -78,7 +99,7 @@ with dpg.window(label='', width=800, height=600, tag = 'Primary Window'):
 dpg.setup_dearpygui()
 dpg.show_viewport()
 thread = threading.Thread(target=update_data)
-dpg.set_primary_window('Primary Window',True)
+dpg.set_primary_window('Primary Window', True)
 thread.start()
 dpg.start_dearpygui()
 dpg.destroy_context()
