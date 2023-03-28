c3ac5190ad0213b90ece773d8eb11836bef3d574
IvanIsak2000 <vingving648@gmail.com>
2023-03-28 13:20:25 +0300

    add second plot

--- main.py
+++ main.py
@@ -27,8 +27,12 @@ global data_x
 data_y = [0.0] * nsamples
 data_x = [0.0] * nsamples
 
+global data_y1
+global data_x1
+data_y1 =[0.0] * nsamples
+data_x1 = [0.0] * nsamples
 
-UPDATE_DELAY = 1
+UPDATE_DELAY = 0.1
 
 
 def get_net_speed():
@@ -43,6 +47,7 @@ def get_net_speed():
     ds = (ds / 1024) / UPDATE_DELAY  # download speed
 
     return (us, ds)
+    print(us,ds)
     bytes_sent, bytes_recv = io_2.bytes_sent, io_2.bytes_recv
 
 
@@ -54,15 +59,28 @@ def update_data():
     while True:
         t = time.time() - t0
         y = float(get_net_speed()[1])
+        y1 = float(get_net_speed()[0])
         data_x.append(t)
         data_y.append(y)
+        data_x1.append(t)
+        data_y1.append(y1)
 
         # set the series x and y to the last nsamples
-        dpg.set_value('series_tag',
+        dpg.set_value('plot1',
                       [list(data_x[-nsamples:]),
                        list(data_y[-nsamples:])])
+
+        dpg.set_value('plot2',[
+                       list(data_x1[-nsamples:]),
+                       list(data_y1[-nsamples:])])
+
         dpg.fit_axis_data('x_axis')
         dpg.fit_axis_data('y_axis')
+
+        dpg.fit_axis_data('x1_axis')
+        dpg.fit_axis_data('y1_axis')
+
+
 
         time.sleep(0.01)
         sample = sample + 1
@@ -93,7 +111,20 @@ with dpg.window(label='', width=800, height=600, tag='Primary Window'):
 
         dpg.add_line_series(x=list(data_x), y=list(data_y),
                             label='Download speed', parent='y_axis',
-                            tag='series_tag')
+                            tag='plot1')
+
+
+        x1_axis = dpg.add_plot_axis(
+            dpg.mvXAxis,
+            label='',
+            tag='x1_axis')
+        y1_axis = dpg.add_plot_axis(dpg.mvYAxis, label='', tag='y1_axis')
+
+        dpg.add_line_series(x=list(data_x1), y=list(data_y1),
+                            label='Upload speed', parent='y1_axis',
+                            tag='plot2')
+
+        
 
 
 dpg.setup_dearpygui()
